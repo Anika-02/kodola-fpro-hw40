84e21d52b645eae1ea557ea020dc960bfee3afe6
anna.kodola <anya.kodola@gmail.com>
2023-10-29 01:51:49 +0300

    added homework 40

---
+++ src/components/CentralContainer/CentralContainer.js
@@ -0,0 +1,11 @@
+import './CentralContainer.css';
+
+function CentralContainer(){
+    return  (
+        <div className='main-container'>
+            <h1>Main Container</h1>
+        </div>
+    );
+}
+
+export default CentralContainer;
---
+++ src/components/LeftSideNavigation/LeftSideNavigation.js
@@ -0,0 +1,11 @@
+import './LeftSideNavigation.css';
+
+function LeftSideNavigation(){
+    return  (
+        <nav>
+            <h2 className="nav-title">LeftSideNavigation</h2>
+        </nav>
+    );
+}
+
+export default LeftSideNavigation;
